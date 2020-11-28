857c28dfbc2dd9e45cfe867da1afdb24ae40547c
Oleksandr Bezushko <34242332+insiderser@users.noreply.github.com>
2020-11-28 15:09:52 +0200

    Add sample from teacher that uses BehaviorSubject for state

---
+++ index.js
@@ -0,0 +1,20 @@
+import TemplateProcessor from './templateProcessor.js'
+import Client from './client.js'
+import Router from './router.js'
+
+const router = new Router()
+const templateProcessor = new TemplateProcessor()
+const client = new Client()
+
+const stateObservable = router.getStateObservable()
+stateObservable.subscribe(({viewName, endpointName}) => {
+    let view
+    import(`./views/${viewName}.js`)
+        .then((viewModule) => {
+            view = viewModule.default
+            return client.getData(endpointName)
+        })
+        .then((data) => {
+            templateProcessor.render(view(data))
+        })
+})
---
+++ router.js
@@ -0,0 +1,37 @@
+const {BehaviorSubject} = rxjs
+
+class Router {
+    /**
+     * @return {*|Observable}
+     */
+    getStateObservable() {
+        const stateSubject = new BehaviorSubject(this.getCurrentState())
+
+        addEventListener('hashchange', () => {
+            const newState = this.getCurrentState()
+            stateSubject.next(newState)
+        })
+
+        return stateSubject
+    }
+
+    getCurrentState() {
+        let viewName = ''
+        let endpointName = ''
+        switch (window.location.hash.split('#')[1]) {
+            case 'products':
+                viewName = 'productsPage'
+                endpointName = 'posts'
+                break
+            default:
+                break
+        }
+
+        return {
+            viewName,
+            endpointName,
+        }
+    }
+}
+
+export default Router
