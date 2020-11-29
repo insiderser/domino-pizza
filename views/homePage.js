db7266963c244ae8c933f527097c347fbb79ff3d
Oleksandr Bezushko <34242332+insiderser@users.noreply.github.com>
2020-11-29 19:32:27 +0200

    Reformat code somewhat

--- views/homePage.js
+++ views/homePage.js
@@ -14,29 +14,38 @@ const view = (data, onCategoryClicked, onProductClicked, onAddToCartClicked) => 
     `)
 
     data.categories
-        .map(categoryWithProducts => {
-            const category = categoryWithProducts.category
-            const products = categoryWithProducts.products
-
-            const categoryDom = parseElementFromString(`
-                <div class="category">
-                    <span class="category-title">${category.name}</span>
-                </div>
-            `)
-
-            categoryDom.onclick = (ev) => {
-                onCategoryClicked(categoryWithProducts)
-                ev.stopPropagation()
-            }
-
-            const productsSection = productsListSection(products, onProductClicked, onAddToCartClicked)
-            categoryDom.appendChild(productsSection)
-
-            return categoryDom
-        })
+        .map(categoryWithProducts => categoryItem(categoryWithProducts, onCategoryClicked, onProductClicked, onAddToCartClicked))
         .forEach(doc => categoriesDom.appendChild(doc))
 
     return categoriesDom
+}
+
+/**
+ * @param {ProductCategoryWithProducts} categoryWithProducts
+ * @param {function(ProductCategoryWithProducts):void} onCategoryClicked
+ * @param {function(Product):void} onProductClicked
+ * @param {function(Product):void} onAddToCartClicked
+ * @return {Element}
+ */
+function categoryItem(categoryWithProducts, onCategoryClicked, onProductClicked, onAddToCartClicked) {
+    const category = categoryWithProducts.category
+    const products = categoryWithProducts.products
+
+    const categoryDom = parseElementFromString(`
+            <div class="category">
+                <span class="category-title">${category.name}</span>
+            </div>
+        `)
+
+    categoryDom.onclick = (ev) => {
+        onCategoryClicked(categoryWithProducts)
+        ev.stopPropagation()
+    }
+
+    const productsSection = productsListSection(products, onProductClicked, onAddToCartClicked)
+    categoryDom.appendChild(productsSection)
+
+    return categoryDom
 }
 
 export default view
--- views/productsListSection.js
+++ views/productsListSection.js
@@ -12,36 +12,44 @@ const view = (products, onProductClicked, onAddToCartClicked) => {
     `)
 
     products
-        .map((product) => {
-            const productItemDocument = parseElementFromString(`
-                <div class="product-item">
-                    <img class="product-image" src="${product.images[0]}" alt="">
-                    <span class="product-name">${product.id}</span>
-                    <span class="product-desc">${product.name}</span>
-                    <div>
-                        <span class="product-price">${product.price} <span class="product-price-currency">uah</span></span>
-                        <button class="add-to-cart">To cart</button>
-                    </div>
-                </div>
-            `)
-
-            productItemDocument.onclick = (ev) => {
-                onProductClicked(product)
-                ev.stopPropagation()
-            }
-
-            for (const addToCartBtn of productItemDocument.getElementsByClassName('add-to-cart')) {
-                addToCartBtn.onclick = (ev) => {
-                    onAddToCartClicked(product)
-                    ev.stopPropagation()
-                }
-            }
-
-            return productItemDocument
-        })
+        .map((product) => productItem(product, onProductClicked, onAddToCartClicked))
         .forEach(document => productsListSection.appendChild(document))
 
     return productsListSection
+}
+
+/**
+ * @param {Product} product
+ * @param {function(Product):void} onProductClicked
+ * @param {function(Product):void} onAddToCartClicked
+ * @return {Element}
+ */
+function productItem(product, onProductClicked, onAddToCartClicked) {
+    const productItemDocument = parseElementFromString(`
+            <div class="product-item">
+                <img class="product-image" src="${product.images[0]}" alt="">
+                <span class="product-name">${product.id}</span>
+                <span class="product-desc">${product.name}</span>
+                <div>
+                    <span class="product-price">${product.price} <span class="product-price-currency">uah</span></span>
+                    <button class="add-to-cart">To cart</button>
+                </div>
+            </div>
+        `)
+
+    productItemDocument.onclick = (ev) => {
+        onProductClicked(product)
+        ev.stopPropagation()
+    }
+
+    for (const addToCartBtn of productItemDocument.getElementsByClassName('add-to-cart')) {
+        addToCartBtn.onclick = (ev) => {
+            onAddToCartClicked(product)
+            ev.stopPropagation()
+        }
+    }
+
+    return productItemDocument
 }
 
 export default view
